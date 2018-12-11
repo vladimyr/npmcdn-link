6ade1bd831c6fc73a0c228257c2d6b2d8c2836ef
Dario Vladovic <d.vladimyr@gmail.com>
2018-12-11 17:09:45 +0100

    Prevent adding multiple "browse files" links :bug:

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import { SidebarSection, Link } from './components';
 import ui, { getVersions, readMetadata } from './ui';
 
 const insertBefore = (el, ref) => ref.parentElement.insertBefore(el, ref);
+const isComponent = el => Boolean(el.dataset.nanocomponent);
 
 const sidebarSection = new SidebarSection();
 onNavigate(() => sidebarSection.render(readMetadata()));
@@ -13,6 +14,7 @@ onTabSelect(tab => {
   const { name } = readMetadata();
   getVersions().forEach(el => {
     const spacer = el.nextElementSibling;
+    if (isComponent(spacer)) return;
     const version = el.textContent.trim();
     const link = new Link();
     insertBefore(link.render({ name, version }), spacer);
