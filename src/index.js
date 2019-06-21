cbdecb0b64a414a97e93ccd6bb983417c460f203
Dario Vladovic <d.vladimyr@gmail.com>
2019-06-21 05:34:17 +0200

    Implement generic render fn

--- src/index.js
+++ src/index.js
@@ -4,9 +4,9 @@ import { getMetadata, getLastSidebarSection, getVersions } from './ui';
 
 let unpkgSection;
 let anvakaSection;
+let versionLinks = [];
 
 const insertBefore = (el, ref) => ref.parentElement.insertBefore(el, ref);
-const isComponent = el => Boolean(el.dataset.nanocomponent);
 
 const unpkgUrl = (name, version) => `https://unpkg.com/${name}@${version}/`;
 const anvakaUrl = (name, version) => `https://npm.anvaka.com/#/view/2d/${name}/${version}`;
@@ -14,44 +14,45 @@ const humanizeUrl = url => url.replace(/^https?:\/\/(?:www\.)?/, '');
 
 injectHook(function onTabRender(_, root) {
   const { activeTab = 'readme' } = getProps(root);
-  decorateSidebar();
-  if (activeTab === 'versions') decorateVersions();
+  const metadata = getMetadata();
+  decorateSidebar(metadata);
+  if (activeTab === 'versions') {
+    decorateVersions(metadata);
+  } else {
+    versionLinks = [];
+  }
 });
 
-function decorateSidebar() {
-  const { name, version } = getMetadata();
-  unpkgSection = render(unpkgSection, {
+function decorateSidebar({ name, version }) {
+  const lastSection = getLastSidebarSection();
+  unpkgSection = render(unpkgSection, SidebarSectionLink, {
     label: 'view contents',
     href: unpkgUrl(name, version),
     text: humanizeUrl(unpkgUrl(name, version))
-  });
-  anvakaSection = render(anvakaSection, {
+  }, lastSection);
+  anvakaSection = render(anvakaSection, SidebarSectionLink, {
     label: 'visualize dependencies',
     href: anvakaUrl(name, version),
     text: humanizeUrl(anvakaUrl(name, version))
-  });
+  }, lastSection);
 }
 
-function decorateVersions() {
-  getVersions().forEach(el => {
-    const spacer = el.nextElementSibling;
-    if (isComponent(spacer)) return;
-    const { name } = getMetadata();
+function decorateVersions({ name }) {
+  getVersions().forEach((el, index) => {
     const version = el.textContent.trim();
-    const link = new VersionLink().render({
+    versionLinks[index] = render(versionLinks[index], VersionLink, {
       text: '[browse files]',
       href: unpkgUrl(name, version)
-    });
-    insertBefore(link, spacer);
+    }, el.nextElementSibling);
   });
 }
 
-function render(section, data) {
-  if (section) {
-    section.render(data);
-    return section;
+function render(component, Ctor, data, ref) {
+  if (component) {
+    component.render(data);
+    return component;
   }
-  section = new SidebarSectionLink();
-  insertBefore(section.render(data), getLastSidebarSection());
-  return section;
+  component = new Ctor();
+  insertBefore(component.render(data), ref);
+  return component;
 }
