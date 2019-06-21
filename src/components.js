1fa417fc787d6551cd6ca3b5832db8c47dc304a5
Dario Vladovic <d.vladimyr@gmail.com>
2019-06-21 03:53:35 +0200

    Cleanup styles :art:

--- src/components.js
+++ src/components.js
@@ -15,10 +15,10 @@ export class SidebarSection extends BaseComponent {
     const href = unpkgUrl(name, version);
     const text = `unpkg.com/${name}@${version}/`;
     return html`
-      <div class="dib w-50 bb b--black-10 pr2 w-100 truncate">
+      <div class="dib w-100 bb b--black-10 pr2">
         <h3 class="f5 mt2 pt2 mb0 black-50">${label}</h3>
         <p class="fw6 mb3 mt2 truncate black-80 f4">
-          <a class="link black-80" href="${href}">${text}</a>
+          <a class="fw6 mb3 mt2 truncate black-80 f4 link" href="${href}">${text}</a>
         </p>
       </div>
     `;
@@ -30,12 +30,7 @@ export class Link extends BaseComponent {
     const href = unpkgUrl(name, version);
     const text = '[browse files]';
     return html`
-      <a
-        class="black-40 code lh-copy link underline-hover"
-        style="margin-left: 0.5rem"
-        href="${href}">
-        ${text}
-      </a>
+      <a class="ml2 black-40 code lh-copy link underline-hover" href="${href}">${text}</a>
     `;
   }
 }
